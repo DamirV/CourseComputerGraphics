030f2d812af5a9ca169614f90e1c9447062c2e14
DamirV <32214748+DamirV@users.noreply.github.com>
2020-07-01 18:26:56 +0300

    Add files via upload

---
+++ RayTracing/RayTracing/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace RayTracing
+{
+    public partial class Form1 : Form
+    {
+        RayTracing scene;
+        public Form1()
+        {
+            InitializeComponent();
+            scene = new RayTracing();
+        }
+
+        private void glControl1_Load(object sender, EventArgs e)
+        {
+            scene.Resize(glControl1.Width, glControl1.Height);
+        }
+        private void glControl1_Paint(object sender, PaintEventArgs e)
+        {
+            scene.Update();
+            glControl1.SwapBuffers();
+            //scene.closeProgram();
+        }
+
+    }
+}
